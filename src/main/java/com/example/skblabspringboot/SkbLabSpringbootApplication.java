62561c446f70f8538ed7b7a0b4738516103867e6
v1nn4ik <v1nn4ik@icloud.com>
2023-03-16 11:23:46 +0500

    init

---
+++ src/main/java/com/example/skblabspringboot/SkbLabSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.skblabspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkbLabSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkbLabSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skblabspringboot/SkbLabSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skblabspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkbLabSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
