58c1a20268ce538926adc099a8ade239ef9a776f
Konstantin <vinnik-03@mail.ru>
2023-03-24 00:28:14 +0500

    попытки сделать дз

--- src/main/java/controller/User.java
+++ src/main/java/controller/User.java
@@ -2,11 +2,15 @@ package controller;
 
 import lombok.Data;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 public class User {
-    @NotBlank(message = "ID is empty")
-    private int id;
+    @NotEmpty(message = "Username is empty")
+    @Size(min = 4, max = 15, message = "Username should be between 4 and 15 characters")
+    private int username;
     @NotBlank(message = "Password is empty")
+    @Size(min = 6, max = 20, message = "Password should be between 6 and 20 characters")
     private String password;
 }
