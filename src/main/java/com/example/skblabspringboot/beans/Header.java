635fd5d4a0d0c093b78463b03dac6081d6e87e61
Konstantin <vinnik-03@mail.ru>
2023-03-26 15:37:02 +0500

    исправления

---
+++ src/main/java/com/example/skblabspringboot/beans/Header.java
@@ -0,0 +1,9 @@
+package com.example.skblabspringboot.beans;
+
+import lombok.Data;
+
+@Data
+public class Header {
+    private String key;
+    private String value;
+}
--- src/main/java/com/example/skblabspringboot/controllers/FisrtController.java
+++ src/main/java/com/example/skblabspringboot/controllers/FisrtController.java
@@ -1,17 +1,23 @@
 package com.example.skblabspringboot.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
 @Slf4j
 public class FisrtController {
     @GetMapping("/headersList")
-    public String headersList(@RequestHeader Map<String, String> headers) {
-        log.info(headers.toString());
-        return String.format("The number of headings: %s", headers.size());
+    public String headersList(@RequestHeader Map<String, String> headers){
+        try {
+            return new ObjectMapper().writeValueAsString(headers);
+        } catch (IOException e) {
+            log.info("Exception :(");
+        }
+        return "";
     }
 }
