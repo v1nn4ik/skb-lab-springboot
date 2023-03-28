0d02b5550739e64e61460d2941f340a02cf1cab9
Konstantin <vinnik-03@mail.ru>
2023-03-28 20:49:43 +0500

    добавил валидацию для user

--- src/main/java/com/example/skblabspringboot/controller/User.java
+++ src/main/java/com/example/skblabspringboot/controller/User.java
@@ -1,13 +1,17 @@
 package com.example.skblabspringboot.controller;
 
 import lombok.Data;
+
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
 public class User {
-    @Size(min=3, max=20)
+    @Size(min=3, max=20, message = "Username must be between 3 and 20 characters")
     private String username;
+    @Email(message = "Email must be correct")
+    private String email;
     @NotBlank(message = "Password is empty")
     private String password;
 }
