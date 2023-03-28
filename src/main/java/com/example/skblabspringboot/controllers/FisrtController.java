d1e33b9da2b7d78bb18c91049bfa16767b6b251f
Konstantin <vinnik-03@mail.ru>
2023-03-28 15:05:49 +0500

    исправил вывод для первого контроллера

--- src/main/java/com/example/skblabspringboot/controllers/FisrtController.java
+++ src/main/java/com/example/skblabspringboot/controllers/FisrtController.java
@@ -1,22 +1,16 @@
 package com.example.skblabspringboot.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.IOException;
 import java.util.Map;
 
 @RestController
 @Slf4j
 public class FisrtController {
     @GetMapping("/headersList")
-    public String headersList(@RequestHeader Map<String, String> headers){
-        try {
-            return new ObjectMapper().writeValueAsString(headers);
-        } catch (IOException e) {
-            return e.getMessage();
-        }
+    public Map<String, String> headersList(@RequestHeader Map<String, String> headers){
+        return headers;
     }
 }
