e396620cbb6b6021229e37fd944d48c1bdda9388
Konstantin <vinnik-03@mail.ru>
2023-04-06 18:44:18 +0500

    сделал конфиги с переменной окружения

--- src/main/java/com/example/skblabspringboot/SkbLabSpringbootApplication.java
+++ src/main/java/com/example/skblabspringboot/SkbLabSpringbootApplication.java
@@ -3,8 +3,6 @@ package com.example.skblabspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class SkbLabSpringbootApplication {
     public static void main(String[] args) {
---
+++ src/main/java/com/example/skblabspringboot/dto/UserDto.java
@@ -0,0 +1,14 @@
+package com.example.skblabspringboot.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String name;
+
+    private String surname;
+
+    private Integer age;
+
+    private String profession;
+}
