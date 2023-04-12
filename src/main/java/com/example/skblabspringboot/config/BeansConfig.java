64dcc271dadde04fa87e9a98914becb1d8951139
Konstantin <vinnik-03@mail.ru>
2023-04-12 20:41:21 +0500

    исправления

--- src/main/java/com/example/skblabspringboot/config/BeansConfig.java
+++ src/main/java/com/example/skblabspringboot/config/BeansConfig.java
@@ -3,15 +3,16 @@ package com.example.skblabspringboot.config;
 
 import com.example.skblabspringboot.dto.BeanDto;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 
 @Configuration
 public class BeansConfig {
     @Bean("testProfile")
-    @ConditionalOnProperty(prefix = "profile", name = "value", havingValue = "test")
+    @Profile("test")
     public BeanDto testProfile() {
         var bean = new BeanDto();
         bean.setName("testBean");
@@ -29,11 +30,11 @@ public class BeansConfig {
     }
 
     @Bean("isMainNotDefault")
-    @ConditionalOnProperty(name = "main", havingValue = "uwu")
+    @ConditionalOnExpression("!'${main}'.equals('default')")
     public BeanDto isMainNotDefault() {
         var bean = new BeanDto();
         bean.setName("isMainNotDefault");
-        System.out.println("MAIN (ENV VAR) is default");
+        System.out.println("MAIN (ENV VAR) isn't default");
         return bean;
     }
 }
--- src/main/java/com/example/skblabspringboot/controller/Controller.java
+++ src/main/java/com/example/skblabspringboot/controller/Controller.java
@@ -3,14 +3,14 @@ package com.example.skblabspringboot.controller;
 
 import com.example.skblabspringboot.dto.UserDto;
 import com.example.skblabspringboot.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/user")
 public class Controller {
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     @PostMapping()
     public UserDto user(@RequestBody UserDto user) {
