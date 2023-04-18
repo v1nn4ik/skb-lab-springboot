0fb56dc9eaba42eb1e0aae9bc7bcd7f38d6ae0bd
Konstantin <vinnik-03@mail.ru>
2023-04-18 16:00:15 +0500

    init

--- src/main/java/com/example/skblabspringboot/SkbLabSpringbootApplication.java
+++ src/main/java/com/example/skblabspringboot/SkbLabSpringbootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkbLabSpringbootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SkbLabSpringbootApplication.class, args);
     }
-
 }
