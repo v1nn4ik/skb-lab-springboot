4112731cf70872654ffd8aab6e13c706fcd15358
Konstantin <vinnik-03@mail.ru>
2023-05-10 17:40:34 +0500

    исправления

--- src/main/java/com/example/skblabspringboot/handler/TransactionalNameEventHandler.java
+++ src/main/java/com/example/skblabspringboot/handler/TransactionalNameEventHandler.java
@@ -3,14 +3,21 @@ package com.example.skblabspringboot.handler;
 import com.example.skblabspringboot.event.DatabaseEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Service
 @Slf4j
 public class TransactionalNameEventHandler {
-    @TransactionalEventListener
-    public void transactionalHandler(DatabaseEvent databaseEvent)
+    @TransactionalEventListener(phase= TransactionPhase.AFTER_COMMIT)
+    public void transactionalHandlerAfterCommit(DatabaseEvent databaseEvent)
     {
-        log.info("Transactional save for {}", databaseEvent.getUserDto().getName());
+        log.info("Transactional save for {} on after commit phase", databaseEvent.getUserDto().getName());
+    }
+
+    @TransactionalEventListener(phase= TransactionPhase.AFTER_ROLLBACK)
+    public void transactionalHandlerAfterRollback(DatabaseEvent databaseEvent)
+    {
+        log.info("Transactional save for {} on after rollback phase", databaseEvent.getUserDto().getName());
     }
 }
